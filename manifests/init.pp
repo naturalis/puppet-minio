00bc7a794c1c359b98b91a3e076ddded15d6ea7e
Atze de Vries <atze.devries@naturalis.nl>
2017-03-09 11:48:30 +0100

    all minio puppet code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,62 @@
 # Class: minio
 # ===========================
 #
-# Full description of class minio here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'minio':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2017 Your name here, unless otherwise noted.
-#
-class minio {
+class minio(
+        $access_key,
+        $secret_key,
+        $ssl_public_cert,
+        $ssl_private_key,
+        $minio_data_dir = '/minio-data'
+){
 
+  $image_name='minio'
+  $container_name='minio-server'
+  $diffcmd = "/usr/bin/diff <(docker image inspect --format='{{.Id}}' ${image_name}) <(docker inspect --format='{{.Image}}' ${container_name})"
+
+  include 'docker'
+
+  file { ['/etc/minio','/etc/minio/certs',$minio_data_dir]
+    ensure => directory,
+  }
+
+  file { '/etc/minio/certs/public.crt' :
+    content => $ssl_public_cert,
+    require => File['/etc/minio/certs'],
+    mode    => '0600',
+  }
+
+  file { '/etc/minio/certs/private.key' :
+    content => $ssl_private_key,
+    require => File['/etc/minio/certs'],
+    mode    => '0600',
+  }
+
+  docker::run { $container_name :
+    image   => $image_name,
+    env     => ["MINIO_ACCESS_KEY=${access_key}",
+                "MINIO_SECRET_KEY=${secret_key}"]
+    volumes => ["${minio_data_dir}:/export",
+                  "/etc/minio:/root/.minio"],
+    command => 'minio/minio server /export',
+    require => [File['/etc/minio/certs/public.crt'],
+                File['/etc/minio/certs/private.key'],
+                File[$minio_data_dir]
+    ]
+  }
+
+  exec { "/bin/systemctl restart docker-${container_name}" :
+    onlyif => $diffcmd,
+    require => [Exec["/usr/bin/docker pull ${image_name}"],Docker::Run[$container_name]]
+  }
+
+  exec {"/usr/bin/docker pull ${image_name}" :
+    schedule => 'everyday',
+  }
+
+  schedule { 'everyday':
+    period  => daily,
+    repeat  => 1,
+    range => "7-9"
+  }
 
 }
